917854d446eeb600be3f66b239871433c6400808
Raj <rajkujur@gmail.com>
2021-03-02 01:29:41 +1100

    second commit

--- src/test/java/extentListerners/ExtentListner.java
+++ src/test/java/extentListerners/ExtentListner.java
@@ -1,11 +1,8 @@
 package extentListerners;
-
 import java.util.Date;
-
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;

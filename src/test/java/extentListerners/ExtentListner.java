d35ad180ad6d5362c6b073563e3f4374f747f6c6
omkaiappi <rathdm19622@gmail.com>
2021-03-02 01:34:08 +1100

    third commit

--- src/test/java/extentListerners/ExtentListner.java
+++ src/test/java/extentListerners/ExtentListner.java
@@ -1,4 +1,5 @@
 package extentListerners;
+
 import java.util.Date;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -8,48 +9,50 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 
-public class ExtentListner implements ITestListener{
+public class ExtentListner implements ITestListener {
 
 	eventManger e = new eventManger();
 	Date d = new Date();
-String filename ="Extent_"+d.toString().replace(":", "_").replace(" ", "_")+".html";
+	String filename = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
 	public ExtentReports reports = eventManger.abc(filename);
+
 	public void onTestStart(ITestResult result) {
-		eventManger.test = eventManger.reports.createTest(result.getClass().getName().toUpperCase() + result.getMethod().getMethodName());
+		eventManger.test = eventManger.reports
+				.createTest(result.getClass().getName().toUpperCase() + result.getMethod().getMethodName());
 	}
 
 	public void onTestSuccess(ITestResult result) {
 		String s = result.getMethod().getMethodName();
-		String text = "Test Case : "+s;
+		String text = "Test Case : " + s;
 		Markup m = MarkupHelper.createLabel(text, ExtentColor.GREEN);
 		eventManger.test.pass(m);
 	}
 
 	public void onTestFailure(ITestResult result) {
 		String s = result.getMethod().getMethodName();
-		String text = "Test Case : "+s;
+		String text = "Test Case : " + s;
 		Markup m = MarkupHelper.createLabel(text, ExtentColor.RED);
 		eventManger.test.fail(m);
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFinish(ITestContext context) {
 		eventManger.reports.flush();
-		
+
 	}
 
 }
